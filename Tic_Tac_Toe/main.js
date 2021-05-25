97e96169ad1143da0168614f2d58eee86cc888b5
Pablo203 <76450441+Pablo203@users.noreply.github.com>
2021-05-25 13:45:41 +0200

    Fixed Tic Tac Toe Mechanic

--- Tic_Tac_Toe/main.js
+++ Tic_Tac_Toe/main.js
@@ -13,7 +13,7 @@ document.getElementById("turn").innerHTML = "Tura: " + turn;
 
 function is_win(){
     if ( //Win dla poziomych linii
-        (field_value[0] && field_value[1] && field_value[2] == X) || (field_value[0] == O && field_value[1] == O && field_value[2] == O) || (field_value[3] == X && field_value[4] == X && field_value[5] == X) || (field_value[3] == O && field_value[4] == O && field_value[5] == O) ||(field_value[6] == X && field_value[7] == X && field_value[8] == X) || (field_value[6] == O && field_value[7] == O && field_value[8] == O) ||
+        (field_value[0] == X && field_value[1] == X && field_value[2] == X) || (field_value[0] == O && field_value[1] == O && field_value[2] == O) || (field_value[3] == X && field_value[4] == X && field_value[5] == X) || (field_value[3] == O && field_value[4] == O && field_value[5] == O) ||(field_value[6] == X && field_value[7] == X && field_value[8] == X) || (field_value[6] == O && field_value[7] == O && field_value[8] == O) ||
 
         //Win dla pionowych linii
         (field_value[0] == X && field_value[3] == X && field_value[6] == X) || (field_value[0] == O && field_value[3] == O && field_value[6] == O) ||(field_value[1] == X && field_value[4] == X && field_value[7] == X) || (field_value[1] == O && field_value[4] == O && field_value[7] == O) ||(field_value[2] == X && field_value[5] == X && field_value[8] == X) || (field_value[2] == O && field_value[5] == O && field_value[8] == O) ||
